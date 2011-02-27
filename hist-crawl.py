5ac076fd5bbe036208c92c053df4ef1d5ea72931
Tom Parker <palfrey@tevp.net>
2011-02-27 21:21:33 +0000

    Initial fully happy version of the history crawler

--- hist-crawl.py
+++ hist-crawl.py
@@ -1,15 +1,35 @@
 import sqlite3
 from re import compile
+from os.path import expanduser, join
+import shutil, tempfile
+from urllib import unquote
+import codecs
+from sys import argv
+from os import system
+
+assert len(argv)==2
 
 enw = compile("http://en.wikipedia.org/wiki/([^#]+)")
 
-conn = sqlite3.connect("History")
+path = expanduser("~/.config/chromium/Default/History")
+
+try:
+	conn = sqlite3.connect(path, timeout=1)
+	conn.execute("select top(1) from visits")
+except sqlite3.OperationalError, e:
+	if str(e).find("database is locked")!=-1:
+		newpath = join(tempfile.gettempdir(), "History")
+		shutil.copyfile(path, newpath)
+		conn = sqlite3.connect(newpath)
 c = conn.cursor()
 
 links = {}
 
-#c.execute("select urls1.url, visits.visit_time, urls2.url from visits,urls as urls1,urls as urls2 where visits.url = urls1.id and visits.from_visit = urls2.id")
 c.execute("select urls1.id,urls1.url, visits1.visit_time, urls2.url from visits as visits1,urls as urls1,urls as urls2,visits as visits2 where visits1.url = urls1.id and visits1.from_visit != 0 and visits1.from_visit = visits2.id and visits2.url = urls2.id")
+
+def cleanup(name):
+	return unquote(name.replace("_", " "))
+
 for row in c:
 	(id, url, time, ref) = row
 
@@ -17,16 +37,23 @@ for row in c:
 	begin = enw.search(ref)
 
 	if begin and end:
-		begin = begin.groups()[0]
-		end = end.groups()[0]
+		begin = cleanup(begin.groups()[0])
+		end = cleanup(end.groups()[0])
+		if begin == end:
+			continue
 		if begin not in links:
 			links[begin] = set()
 		links[begin].add(end)
 
-print "digraph wikipedia {"
+dotfile = join(tempfile.gettempdir(), "history-dot")
+out = codecs.open(dotfile, "wb", "utf-8")
+
+print >>out, "digraph wikipedia {"
+print >>out, "graph [overlap=\"false\", sep=\"+8,8\"];"
 
 for begin in sorted(links):
 	for end in sorted(links[begin]):
-		print "%s -> %s;"%(begin, end)
+		print >>out, "\"%s\" -> \"%s\";"%(begin, end)
 
-print "}"
+print >>out, "}"
+system("twopi %s -Tps -o %s"%(dotfile,argv[1]))
